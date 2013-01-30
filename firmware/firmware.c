fb864c7b407e148503eb6e319332d1fd3c037f3f
Samuel Werder <swerder+git@gmail.com>
2013-01-30 23:31:24 +0100

    Firmware Version 0.0.2

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -7,71 +7,12 @@
 #include <softPwm.h>
 #include <string.h>
 
-
-//Pin defination for the SPI
-#define MOSI	10
-#define MISO	9
-#define SCLK	11
-#define CS	8
-//Pin defination for the I2C
-#define SDA	      2
-#define SCL	      3
-//AD774 Register defination
-#define READ_STATUS_REG		0x40
-#define READ_MODE_REG		0x48
-#define WRITE_MODE_REG		0x08
-#define READ_STRUCT_REG		0x50
-#define WRITE_STRUCT_REG	0x10
-#define READ_DATA_REG		0x58
-#define READ_SERIAL_REG		0x60
-#define READ_IO_REG 		0x68
-#define WRITE_IO_REG		0x28
-#define READ_SHIFT_REG		0x70
-//I2C_DELAY_TIME
-#define DELAY_TIME   10
-
-
-//virtualSPI function delaration
-void VirtualSPIInit(void);
-void SPIReset(void);
-void SPIWrite(uint8_t data);	
-uint8_t SPIRead(void);
-void SPIWriteByte(uint8_t reg, uint8_t data);
-void SPIWrite2Bytes(uint8_t reg, uint32_t data);
-uint8_t SPIReadByte(uint8_t register);
-uint32_t SPIRead2Bytes(uint8_t register);
-uint32_t SPIRead3Bytes(uint8_t register);
-//virtualI2C function delaration
-void VirtualI2CInit(void);
-void I2CStart(void);
-void I2CStop(void);
-int CheckAck(void);
-uint8_t I2CReadByte(void);
-void I2CWriteByte(uint8_t data);
-void I2CWriteBytes(uint8_t *data, uint8_t len);
-//GPIO PCA9685 initialization
-void GPIOInit(void);
-void AD7794Init(void);
-void PCA9685Init(void);
-
-
-
-
-uint32_t readADC(uint8_t i);
-uint32_t readSignPin(uint8_t i);
-uint32_t readRaspberryPin(uint8_t i);
-void writeControllButtonPin(uint8_t i, uint8_t on);
-void writeRaspberryPin(uint8_t i, uint8_t on);
-void buzzer(uint8_t on, uint32_t pwm);
-void writeI2CPin(uint8_t i, uint32_t value);
+#include "basic_functions.h"
+#include "firmware.h"
 
 
 
 //Other functions
-void ProcessCommand(void);
-void WriteFile(void);
-void ReadFile(void);
-void ReadConfigurationFile(void);
 void NumberConvertToString(uint32_t num, char *str);
 void StringClean(char *str, uint32_t len);
 void StringUnion(char *fristString, char *secondString);
@@ -83,20 +24,6 @@ int POWNTimes(uint32_t num, uint8_t n);
 char TotalUpdate[512];
 
 //HardwareTimer FreqTimer(3); //timer 2 conflicts with servo!!!
-
-
-void OptionControl();
-void TempControl();
-void PressControl();
-void MotorControl();
-void ValveControl();
-void ScaleFunction();
-void setFreqTimer();
-void FreqHandler(); 
-void SegmentDisplay();
-void SegmentDisplaySimple();
-void SegmentDisplayOptimized();
-void Beep();
 
 
 
@@ -236,41 +163,6 @@ The Modes:
 
 
 
-uint32_t ConfigurationReg[] = {0x710, 0x711, 0x712, 0x713, 0x714, 0x115};//For AD7794 conf-reg
-
-uint8_t PinNumSign[] = {24, 25, 27, 22, 7, 17};
-
-uint8_t PinNumControllButtons[] = {14, 15, 18, 23};
-
-const int IND_KEY1 = 0; //14
-const int IND_KEY2 = 1; //15
-const int IND_KEY3 = 2; //18
-const int IND_KEY4 = 3; //23
-
-const int ADC_WEIGHT1 = 0;
-const int ADC_WEIGHT2 = 1;
-const int ADC_WEIGHT3 = 2;
-const int ADC_WEIGHT4 = 3;
-const int ADC_PRESS = 4;
-const int ADC_TEMP = 5;
-
-const int I2C_MOTOR = 0;
-const int I2C_SERVO = 1;
-const int I2C_7SEG_TOP = 2;				//SegAPin
-const int I2C_7SEG_TOP_LEFT = 3;		//SegBPin
-const int I2C_7SEG_TOP_RIGHT = 4;		//SegFPin
-const int I2C_7SEG_CENTER = 5;			//SegGPin
-const int I2C_7SEG_BOTTOM_LEFT = 6;		//SegEPin
-const int I2C_7SEG_BOTTOM_RIGHT = 7;	//SegCPin
-const int I2C_7SEG_BOTTOM = 8;			//SegDPin
-const int I2C_7SEG_PERIOD = 9;			//SegDPPin
-
-const int PI_PIN_BUZZER = 4; //7?
-const int BUZZER_PWM = 1000;
-
-const char *commandFile = "/var/www/writefile.txt";
-const char *statusFile = "/var/www/readfile.txt";
-
 
 unsigned int setTemp = 0;
 unsigned int setPress = 0;
@@ -304,59 +196,11 @@ char oldSegmentDisplay = ' ';
 unsigned int blickState = 0;
 
 
-
-const int MIN_COOK_MODE = 10;
-const int MAX_COOK_MODE = 29;
-
-const int MIN_TEMP_MODE = 10;
-const int MAX_TEMP_MODE = 19;
-
-const int MIN_PRESS_MODE = 20;
-const int MAX_PRESS_MODE = 29;
-
-const int MIN_STATUS_MODE = 30;
-const int MAX_STATUS_MODE = 39;
-
-const int MIN_ERROR_MODE = 40;
-const int MAX_ERROR_MODE = 49;
-
-
-const int MODE_STANDBY = 0;
-const int MODE_CUT = 1;			//STIME, MORPM(, MOON, MOOFF)
-const int MODE_SCALE = 2;		//W0, STIME
-
-const int MODE_HEADUP = 10; 	//T0, STIME, MORPM, MOON, MOOFF
-const int MODE_COOK = 11; 		//T0, STIME, MORPM, MOON, MOOFF
-const int MODE_COOLDOWN = 12; 	//T0, STIME, MORPM, MOON, MOOFF
-
-const int MODE_PRESSUP = 20;	//P0, STIME, MORPM, MOON, MOOFF
-const int MODE_PRESSHOLD = 21;	//P0, STIME, MORPM, MOON, MOOFF
-const int MODE_PRESSDOWN = 22;	//P0, STIME, MORPM, MOON, MOOFF
-const int MODE_PRESSVENT = 23;	//P0, STIME, MORPM, MOON, MOOFF
-
-const int MODE_HOT = 30;
-const int MODE_PRESSURIZED = 31;
-const int MODE_COLD = 32;
-const int MODE_PRESSURELESS = 33;
-const int MODE_WEIGHT_REACHED = 34;
-const int MODE_COOK_TIMEEND = 35;
-const int MODE_RECIPE_END = 39;
-
-const int MODE_INPUT_ERROR = 40;
-const int MODE_EMERGANCY_SHUTDOWN = 41;
-const int MODE_MOTOR_OVERLOAD = 42;
-
-const int MODE_WATCHDOW = 50;
-const int MODE_COMMUNICATION_ERROR = 53;
+const char *commandFile = "/var/www/writefile.txt";
+const char *statusFile = "/var/www/readfile.txt";
 
 
 
-const int MODE_OPTIONS_BEGIN = 100;
-const int MODE_OPTION_REMEMBER_BEEP_ON = 100;
-const int MODE_OPTION_REMEMBER_BEEP_OFF = 101;
-
-//$#define DEBUG_ENABLED = 1;
-const int DEBUG_ENABLED = 1;
 
 
 
@@ -385,11 +229,7 @@ const int DEBUG_ENABLED = 1;
 
 
 int main(void){
-	VirtualSPIInit();
-	VirtualI2CInit();
-	GPIOInit();
-	PCA9685Init();
-	AD7794Init();
+	initHardware();
 	delay(30);
 	StringClean(TotalUpdate, 512);
 	ReadConfigurationFile();
@@ -1200,6 +1040,7 @@ uint32_t StringConvertToNumber(char *str)
  */
 void ReadConfigurationFile(void)
 {
+/*
 	FILE *fp;
 	char tempString[10];
 	uint8_t i = 0;
@@ -1236,6 +1077,7 @@ void ReadConfigurationFile(void)
 		}
 	}
 	fclose(fp);
+*/
 }
 /*
 */
@@ -1286,360 +1128,6 @@ int POWNTimes(uint32_t num, uint8_t n)
 
 
 
-
-
-uint8_t Data[10];
-
-/*******************PI Driver Code**********************/
-/* VirtualSPIInit
- */
-void VirtualSPIInit(void){
-	wiringPiSetupGpio();
-	
-	pinMode(MOSI, OUTPUT);
-	pinMode(MISO, INPUT);
-	pinMode(SCLK, OUTPUT);
-	pinMode(CS, OUTPUT);	
-	delay(30);
-}
-/* SPIReset: Reset the AD7794 chip, write 4 0xff.
- *
- */
-void SPIReset(void){
-	digitalWrite(CS, LOW);
-	SPIWrite(0xff);
-	SPIWrite(0xff);
-	SPIWrite(0xff);
-	SPIWrite(0xff);
-	digitalWrite(CS, HIGH);	
-}
-/* SPIWrite: Write one byte data to the register in AD7794
- * data:datas write to register
- */
-void SPIWrite(uint8_t data){
-	int i = 7;
-	
-	for (i = 7; i >= 0; i--){
-		digitalWrite(SCLK, LOW);	
-		if (data & (1<<i)){
-			digitalWrite(MOSI, HIGH);
-		} else {
-			digitalWrite(MOSI, LOW);
-		}
-		delayMicroseconds(100);
-		digitalWrite(SCLK, HIGH);
-		delayMicroseconds(100);		
-	}	
-}
-/* SPIRead: Read one byte data from the AD7794
- * return: data from register
- */
-uint8_t SPIRead(void){
-	int i = 7;
-	uint8_t temp = 0;
-	
-	for (i = 7; i >= 0; i--){
-		digitalWrite(SCLK, LOW);
-		delayMicroseconds(100);
-		digitalWrite(SCLK, HIGH);
-		delayMicroseconds(100);
-		if (digitalRead(MISO)){
-			temp = temp | (1<<i);
-		}
-	}
-
-	return temp;
-}
-/* SPIWriteByte: Wirte one byte to denstination register. 
- * 
- */
-void SPIWriteByte(uint8_t reg, uint8_t data){
-	digitalWrite(CS, LOW);
-	SPIWrite(reg);
-	SPIWrite(data);
-	digitalWrite(CS, HIGH);
-}
-/* SPIWrite2Bytes: Wirte one byte to denstination register. 
- * 
- */
-void SPIWrite2Bytes(uint8_t reg, uint32_t data){
-	digitalWrite(CS, LOW);
-	SPIWrite(reg);
-	SPIWrite((data>>8)&0xff);
-	SPIWrite(data&0xff);
-	digitalWrite(CS, HIGH);
-}
-/* SPIReadByte: Get one byte from denstination register. 
- * return: 1 byte data
- */
-uint8_t SPIReadByte(uint8_t reg){
-	uint8_t data;
-
-	digitalWrite(CS, LOW);
-	SPIWrite(reg);
-	data = SPIRead();
-	digitalWrite(CS, HIGH);
-
-	return data;
-}
-/* SPIReadByte: Get two bytes from denstination register. 
- * return: 2 bytes data
- */
-uint32_t SPIRead2Bytes(uint8_t reg){
-	uint32_t data;
-
-	digitalWrite(CS, LOW);
-	SPIWrite(reg);
-	data = SPIRead();
-	data = (data << 8)| SPIRead();
-	digitalWrite(CS, HIGH);
-	
-	return data;
-}
-/* SPIReadByte: Get three bytes from denstination register. 
- * return: 3 bytes data
- */
-uint32_t SPIRead3Bytes(uint8_t reg){
-	uint32_t data;
-
-	digitalWrite(CS, LOW);
-	SPIWrite(reg);
-	data = SPIRead();
-	data = (data << 8)| SPIRead();
-	data = (data << 8)| SPIRead();
-	digitalWrite(CS, HIGH);
-
-	return data;
-}
-/* VirtualI2CInit: Initializate the virtual I2C-BUS protocal
- *
- */
-void VirtualI2CInit(void){
-	wiringPiSetupGpio();
-	pinMode(SDA, OUTPUT);
-	pinMode(SCL, OUTPUT);
-}
-/* I2CStart: Simulate the I2C start
- *
- */ 
-void I2CStart(void){
-	digitalWrite(SDA, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SCL, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SDA, LOW);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SCL, LOW);
-	delayMicroseconds(DELAY_TIME);
-}
-/* I2CStop: Simulate the I2C stop
- *
- */
-void I2CStop(void){
-	digitalWrite(SDA, LOW);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SCL, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SDA, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SCL, LOW);
-	delayMicroseconds(DELAY_TIME);
-}
-/* CheckAck: Checking Acknowleage in the SDA
- * return:1- Got the ACK, 0-Did got the ACK
- */
-int CheckAck(void){
-	int temp;
-	
-	digitalWrite(SDA, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	digitalWrite(SCL, HIGH);
-	delayMicroseconds(DELAY_TIME);
-	pinMode(SDA, INPUT);
-	delayMicroseconds(1);
-	temp = digitalRead(SDA);
-	pinMode(SDA, OUTPUT);
-	digitalWrite(SCL, LOW);
-	delayMicroseconds(DELAY_TIME);
-	if (temp == 1){
-		return 0;
-	}
-	return 1;
-}
-/* I2CWriteByte: Write one Byte to the PCA9685
- * data:Data to the PCA9685
- */
-void I2CWriteByte(uint8_t data){
-	uint8_t i = 0;
-
-	for (; i < 8; ++i){
-		if ((data&0x80)==0x80){
-			digitalWrite(SDA, HIGH);
-		} else {
-			digitalWrite(SDA, LOW);
-		}
-		data = data << 1;
-		delayMicroseconds(DELAY_TIME);		
-		digitalWrite(SCL, HIGH);
-		delayMicroseconds(DELAY_TIME);
-		digitalWrite(SCL, LOW);
-		delayMicroseconds(DELAY_TIME);
-	}
-	if (!CheckAck()){
-		I2CStop();
-	}
-}
-/* I2CReadByte: Read one Byte from the PCA9685
- * return:Data from the PCA9685
- */
-uint8_t I2CReadByte(void){
-	uint8_t i = 0, temp = 0, data = 0;
-
-	for (; i < 8; ++i){
-		digitalWrite(SDA, HIGH);
-		delayMicroseconds(DELAY_TIME);
-		digitalWrite(SCL, HIGH);
-		pinMode(SDA, INPUT);
-		delayMicroseconds(1);
-		temp = digitalRead(SDA);
-		pinMode(SDA, OUTPUT);
-		delayMicroseconds(DELAY_TIME);
-		digitalWrite(SCL, LOW);
-		if (temp == 1){
-			data = data << 1;
-			data = data | 0x01;
-		} else {
-			data = data << 1;
-		}
-	}
-	digitalWrite(SCL, LOW);
-	return data;
-}
-/* I2CWriteBytes: Write one Byte to the PCA9685
- * data:The frist byte must be the address of the 'CHIP', then the data to the
- *      chip.
- */
-void I2CWriteBytes(uint8_t *data, uint8_t len){
-	uint8_t i = 0;
-
-	I2CStart();
-	for (; i < len; ++i){
-		I2CWriteByte(*data++);
-	}
-	I2CStop();
-	delay(3);
-}
-void GPIOInit(void){
-	pinMode(24 ,INPUT);	//SIG1
-	pinMode(25 ,INPUT);	//SIG2
-	pinMode(27 ,INPUT);	//SIG3
-	pinMode(22 ,INPUT);	//SIG4
-	pinMode(7 ,INPUT);	//SIG5
-	pinMode(17 ,INPUT);	//SIG6
-
-	pinMode(14, OUTPUT);	//KEY1
-	pinMode(15, OUTPUT);	//KEY2
-	pinMode(18, OUTPUT);	//KEY3
-	pinMode(23, OUTPUT);	//KEY4
-	
-}
-void PCA9685Init(void){
-	Data[0] = 0x80;
-	Data[1] = 0x00;
-	Data[2] = 0x31;
-	I2CWriteBytes(Data, 3);
-	Data[1] = 0xfe;
-	Data[2] = 0x04;
-	I2CWriteBytes(Data, 3);
-	Data[1] = 0x00;
-	Data[2] = 0xa1;
-	I2CWriteBytes(Data, 3);
-	Data[1] = 0x01;
-	Data[2] = 0x04;
-	I2CWriteBytes(Data, 3);
-}
-void AD7794Init(void){
-	SPIReset();	
-	delay(30);
-	SPIWrite2Bytes(WRITE_MODE_REG, 0x0002);
-}
-
-
-
-
-
-uint32_t readADC(uint8_t i){
-	uint32_t temp;
-	SPIWrite2Bytes(WRITE_STRUCT_REG, ConfigurationReg[i]);
-	delay(50);
-	temp = SPIRead3Bytes(READ_DATA_REG);
-	if (DEBUG_ENABLED){printf("readADC(%d): %06X\n", i, temp);}
-	return temp;
-}
-
-uint32_t readSignPin(uint8_t i){
-	uint32_t temp;
-	temp = digitalRead(PinNumSign[i]);
-	if (DEBUG_ENABLED){printf("readSignPin(%d): %06X\n", i, temp);}
-	return temp;
-}
-
-uint32_t readRaspberryPin(uint8_t i){
-	uint32_t temp;
-	temp = digitalRead(i);
-	if (DEBUG_ENABLED){printf("readRaspberryPin(%d): %06X\n", i, temp);}
-	return temp;
-}
-
-void writeControllButtonPin(uint8_t i, uint8_t on){
-	if (DEBUG_ENABLED){printf("writeControllButtonPin(%d): %d\n", i, on);}
-	if (on){
-		digitalWrite(PinNumControllButtons[i], HIGH);
-	} else {
-		digitalWrite(PinNumControllButtons[i], LOW);
-		/*
-		delay(500);
-		digitalWrite(PinNumControllButtons[i], HIGH);
-		*/
-	}
-}
-
-void writeRaspberryPin(uint8_t i, uint8_t on){
-	if (DEBUG_ENABLED){printf("writeRaspberryPin(%d): %d\n", i, on);}
-	if (on){
-		digitalWrite(i, HIGH);
-	} else {
-		digitalWrite(i, LOW);
-		/*
-		//TODO set back to high, but not here, to long delay while running firmware...
-		delay(500);
-		digitalWrite(i, HIGH);
-		*/
-	}
-}
-
-void buzzer(uint8_t on, uint32_t pwm){
-	if (DEBUG_ENABLED){printf("buzzer(=pin %d): on:%d pwm:%d\n", PI_PIN_BUZZER, on, pwm);}
-	if (on){
-		softPwmWrite(PI_PIN_BUZZER, 0);
-	} else {
-		softPwmCreate(PI_PIN_BUZZER, 0, pwm);
-		softPwmWrite(PI_PIN_BUZZER, pwm/10);
-	}
-}
-
-
-void writeI2CPin(uint8_t i, uint32_t value){
-	if (DEBUG_ENABLED){printf("writeI2CPin(%d): %04X\n", i, value);}
-	//Data[0] = 0x80; //set in PCA9685Init()
-	Data[1] = 0x06+i*4;
-	Data[2] = 0x00;
-	Data[3] = 0x00;//value&0xff;
-	Data[4] = value&0xff;
-	Data[5] = (value>>8)&0xff;
-	I2CWriteBytes(Data, 6);		
-	delay(3);
-}
 
 
 		
---
+++ firmware/firmware.h
@@ -0,0 +1,75 @@
+/* Header of the firmware.
+ *
+ */
+
+#define MIN_COOK_MODE	10
+#define MAX_COOK_MODE	29
+
+#define MIN_TEMP_MODE	10
+#define MAX_TEMP_MODE	19
+
+#define MIN_PRESS_MODE	20
+#define MAX_PRESS_MODE	29
+
+#define MIN_STATUS_MODE	30
+#define MAX_STATUS_MODE	39
+
+#define MIN_ERROR_MODE	40
+#define MAX_ERROR_MODE	49
+
+
+#define MODE_STANDBY				0
+#define MODE_CUT					1			//STIME, MORPM(, MOON, MOOFF)
+#define MODE_SCALE					2		//W0, STIME
+
+#define MODE_HEADUP					10 	//T0, STIME, MORPM, MOON, MOOFF
+#define MODE_COOK					11 		//T0, STIME, MORPM, MOON, MOOFF
+#define MODE_COOLDOWN				12 	//T0, STIME, MORPM, MOON, MOOFF
+
+#define MODE_PRESSUP				20	//P0, STIME, MORPM, MOON, MOOFF
+#define MODE_PRESSHOLD				21	//P0, STIME, MORPM, MOON, MOOFF
+#define MODE_PRESSDOWN				22	//P0, STIME, MORPM, MOON, MOOFF
+#define MODE_PRESSVENT				23	//P0, STIME, MORPM, MOON, MOOFF
+
+#define MODE_HOT					30
+#define MODE_PRESSURIZED			31
+#define MODE_COLD					32
+#define MODE_PRESSURELESS			33
+#define MODE_WEIGHT_REACHED			34
+#define MODE_COOK_TIMEEND			35
+#define MODE_RECIPE_END				39
+
+#define MODE_INPUT_ERROR			40
+#define MODE_EMERGANCY_SHUTDOWN		41
+#define MODE_MOTOR_OVERLOAD			42
+
+#define MODE_WATCHDOW				50
+#define MODE_COMMUNICATION_ERROR	53
+
+
+
+#define MODE_OPTIONS_BEGIN					100
+#define MODE_OPTION_REMEMBER_BEEP_ON		100
+#define MODE_OPTION_REMEMBER_BEEP_OFF		101
+
+
+
+void ProcessCommand(void);
+void WriteFile(void);
+void ReadFile(void);
+void ReadConfigurationFile(void);
+
+
+void OptionControl();
+void TempControl();
+void PressControl();
+void MotorControl();
+void ValveControl();
+void ScaleFunction();
+void setFreqTimer();
+void FreqHandler(); 
+void SegmentDisplay();
+void SegmentDisplaySimple();
+void SegmentDisplayOptimized();
+void Beep();
+
